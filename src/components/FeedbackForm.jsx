1199a4b31890e693be3c0f641d8b037992136c56
Arun <kashyapar002@gmail.com>
2022-11-04 21:39:49 +0530

    commit

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Card from "./shared/Card";
+import { useState, useContext, useEffect } from "react";
+import Button from "./shared/Button";
+import FeedbackContext from "../context/FeedbackContext";
+import RatingSelect from "./RatingSelect";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(10);
+
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
+
+  // validation 
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be atleasst 10 Characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
+      }
+
+      setText("");
+    }
+  };
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How Would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)}></RatingSelect>
+        <div className="input-group">
+          <input
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" version="primary" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {/* message to be displayed based on our validation */}
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,100 @@
+import { createContext, useState, useEffect } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [feedback, setFeedback] = useState([]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `/feedback?_sort=id&_order=desc`
+    );
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
+  // I have used proxy in my package.json which is used for every request
+
+  // Add feedback
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
+
+  };
+
+  // Delete feedback
+  const deleteFeedback = async (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
+
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  //Update Feedback Item
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
+
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
+    );
+
+  };
+
+  //Set Item to be Updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        isLoading,
+        feedbackEdit,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
